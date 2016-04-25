73ec706aca4f8e9e117e8821db5f1e8e021fc6de
zzz <617203748@qq.com>
2016-04-25 15:17:56 +0800

    Fragment 1 Banner列表和Goods列表

--- app/src/main/java/com/zgp/fragment/Fragment_1.java
+++ app/src/main/java/com/zgp/fragment/Fragment_1.java
@@ -1,19 +1,23 @@
 package com.zgp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -24,15 +28,17 @@ import com.zgp.bean.Bean_Goods;
 import com.zgp.http.HttpController;
 import com.zgp.http.IRequestCallBack;
 import com.zgp.utils.MyLogger;
+import com.zgp.utils.Util_System;
 import com.zgp.view.ToastView;
 import com.zgp.zgpapp.R;
 
 import net.tsz.afinal.FinalHttp;
-import net.tsz.afinal.http.AjaxCallBack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by 61720 on 2016/4/13.
@@ -59,13 +65,26 @@ public class Fragment_1 extends BaseFragment implements View.OnClickListener {
     /**
      * 广告相关
      */
-    private List<Bean_Banner> bannerList;
+    private List<Bean_Banner> mNewsList;
     private ViewPager vp_banner;// 广告banner
     private LinearLayout ll_dot;// 广告右下角 点
     private TextView tv_newsTitle;// 广告标题
     private int currentItem = 0; // 当前图片的索引号
     private boolean isTaskRun;//当用户滑动中暂停自动滑动任务，提高UI交互效果
     private ScheduledExecutorService scheduledExecutorService;//定时任务
+    private LayoutInflater inflater;
+    // 显示图片的imageView
+    private List<ImageView> mViewList = new ArrayList<ImageView>();
+    // 显示点的imageView
+    private List<ImageView> dots = new ArrayList<ImageView>();
+
+    // 切换Banner当前显示的图片
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            vp_banner.setCurrentItem(currentItem);// 切换当前显示的图片
+        }
+
+    };
 
 
     @Override
@@ -73,6 +92,7 @@ public class Fragment_1 extends BaseFragment implements View.OnClickListener {
         super.onCreate(savedInstanceState);
 
         finalHttp = new FinalHttp();
+        inflater = LayoutInflater.from(getActivity());
 
         imageLoader.init(ImageLoaderConfiguration.createDefault(getActivity()));
         bannerOptions = new DisplayImageOptions.Builder().showImageOnLoading(R.drawable.logo) // 设置图片下载期间显示的图片
@@ -108,13 +128,14 @@ public class Fragment_1 extends BaseFragment implements View.OnClickListener {
         HttpController.post(UrlPath.GetBannerList, null, new IRequestCallBack() {
             @Override
             public void onSuccess(String result) {
-                bannerList = JSON.parseArray(result, Bean_Banner.class);
-                if (bannerList != null && bannerList.size() > 0) {
+                mNewsList = JSON.parseArray(result, Bean_Banner.class);
+                if (mNewsList != null && mNewsList.size() > 0) {
+                    initViewPager();
                 } else {
-                    bannerList = new ArrayList<Bean_Banner>();
+                    mNewsList = new ArrayList<Bean_Banner>();
                 }
 
-                MyLogger.log("bannerList.size：" + bannerList.size());
+                MyLogger.log("mNewsList.size：" + mNewsList.size());
             }
 
             @Override
@@ -154,5 +175,157 @@ public class Fragment_1 extends BaseFragment implements View.OnClickListener {
         ToastView toastView = new ToastView(getActivity(), str);
         toastView.setGravity(Gravity.CENTER, 0, 0);
         toastView.show();
+    }
+
+    private void initViewPager() {
+        float density = Util_System.getDensity(getActivity());
+        int length = mNewsList.size();
+
+        for (int i = 0; i < length; i++) {
+            ImageView view = (ImageView) inflater.inflate(R.layout.banner_item, null);
+            final int index = i;
+            view.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast("点击了：" + mNewsList.get(index).getId());
+                }
+            });
+            mViewList.add(view);
+        }
+
+        // 设置点
+        for (int i = 0; i < mNewsList.size(); i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams((int) (10 * density), (int) (10 * density)));
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(imageView.getLayoutParams());
+            lp.setMargins((int) (2.5 * density), 0, (int) (2.5 * density), 0);
+            imageView.setLayoutParams(lp);
+            if (i == 0) {
+                imageView.setImageResource(R.drawable.dot_focused);
+            } else {
+                imageView.setImageResource(R.drawable.dot_normal);
+            }
+            dots.add(imageView);
+            ll_dot.addView(imageView);
+        }
+        //设置文字
+        for (int i = 0; i < mNewsList.size(); i++) {
+            if (i == 0) {
+                tv_newsTitle.setText(mNewsList.get(0).getTitle());
+            }
+        }
+        if (mViewList != null && mViewList.size() > 0) {
+            vp_banner.setAdapter(new ViewPagerAdapter());
+        }
+        vp_banner.setOnPageChangeListener(new PageChangeListener());
+        vp_banner.setCurrentItem(0);
+        //开启自动轮播任务
+        startTask();
+    }
+
+    /**
+     * 布局适配器，设置数据
+     *
+     * @author xk
+     */
+    private class ViewPagerAdapter extends PagerAdapter {
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView(mViewList.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            if (mViewList.get(position).getParent() == null) {
+                ((ViewPager) container).addView(mViewList.get(position));
+            } else {
+                ((ViewGroup) (mViewList.get(position).getParent())).removeView(mViewList.get(position));
+                ((ViewPager) container).addView(mViewList.get(position));
+            }
+            imageLoader.displayImage(UrlPath.IMAGE_URL + mNewsList.get(position).getImgUrl(), mViewList.get(position), bannerOptions);
+            return mViewList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mViewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+    }
+
+    private class PageChangeListener implements ViewPager.OnPageChangeListener {
+        /* state:0空闲，1是滑行中，2加载完毕*/
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            if (state == 0 && !isTaskRun) {
+                startTask();
+            } else if (state == 1 && isTaskRun) {
+                stopTask();
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            currentItem = position;
+            tv_newsTitle.setText(mNewsList.get(position).getTitle());
+            setDotImage(position);
+        }
+    }
+
+    private void setDotImage(int position) {
+        for (int i = 0; i < dots.size(); i++) {
+            if (i == position) {
+                dots.get(i).setImageResource(R.drawable.dot_focused);
+            } else {
+                dots.get(i).setImageResource(R.drawable.dot_normal);
+            }
+        }
+    }
+
+    /**
+     * 换行切换任务
+     *
+     * @author Administrator
+     */
+    private class ScrollTask implements Runnable {
+
+        public void run() {
+            synchronized (vp_banner) {
+                currentItem = (currentItem + 1) % mViewList.size();
+                handler.obtainMessage().sendToTarget(); // 通过Handler切换图片
+            }
+        }
+    }
+
+    /**
+     * 开启定时任务
+     */
+    private void startTask() {
+        //开启任务
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        // 当Activity显示出来后，每两秒钟切换一次图片显示
+        // command：执行线程 initialDelay：初始化延时 period：两次开始执行最小间隔时间 unit：计时单位
+        scheduledExecutorService.scheduleAtFixedRate(new ScrollTask(), 3, 3, TimeUnit.SECONDS);
+        isTaskRun = true;
+    }
+
+    /**
+     * 停止定时任务
+     */
+    private void stopTask() {
+        isTaskRun = false;
+        scheduledExecutorService.shutdown();
     }
 }
